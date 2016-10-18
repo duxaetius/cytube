8556195daffb130549243e794595629199bfdd42
duxaetius <orchomenus@gmx.com>
2016-10-18 10:44:21 -0700

    Update channelbase.js

--- channelbase.js
+++ channelbase.js
@@ -710,7 +710,7 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', 'UA-53755606-1', 'auto');
 ga('send', 'pageview');
 
-//var bgColorArray = ['//www.dream-wallpaper.com/free-wallpaper/nature-wallpaper/nordic-scenery-wallpaper/1920x1200/free-wallpaper-2.jpg'],
+//var bgColorArray = ['//dream-wallpaper.com/free-wallpaper/nature-wallpaper/nordic-scenery-wallpaper/1920x1200/free-wallpaper-2.jpg'],
     selectBG = bgColorArray[Math.floor(Math.random() * bgColorArray.length)];
 $('#backg').css('background', 'url(' + selectBG + ')')
 var vplayer = videojs("ytapiplayer")
